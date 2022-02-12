aaa9f702d57c427d2604da8e3ed717e2df997d2b
Piotr <szlachcic49er@gmail.com>
2022-02-12 15:50:40 +0100

    Add check imu and battery voltage

--- leo_fw/src/leo_fw/validate.py
+++ leo_fw/src/leo_fw/validate.py
@@ -1,5 +1,10 @@
+from asyncio.constants import ACCEPT_RETRY_DELAY
+from pickle import TRUE
+from posixpath import isabs
 from typing import Optional
 from enum import Enum
+
+import time
 
 import rospy
 import rosgraph
@@ -11,16 +16,35 @@ import yaml
 from .utils import *
 from .board import BoardType, determine_board, check_firmware_version
 from leo_msgs.msg import Imu, WheelOdom, WheelStates
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float32 
 
 rospack = rospkg.RosPack()
 
-class TestMode(Enum):
-    GROUND_WHEEL = "ground_wheel"
-    FLOAT_WHEEL = "float_wheel"
-    NO_WHEEL = "no_wheel"
+###YAML PARSER
 
-    def __str__(self):
-        return self.value
+def parse_yaml(file_path):
+    with open(file_path, "r") as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    return {}
+
+path = rospack.get_path('leo_fw')
+motor_valid = parse_yaml(path+"/validate/motor.yaml")
+
+imuData = Imu()
+wheelData = WheelStates()
+batteryData = Float32()
+
+isNewImuData = 0
+isNewWheelData = 0
+isNewBatteryData = 0
+
+isWheelLoaded = 0
+wheelSpeedLimit = 0.05
 
 class TestHW(Enum):
     HBRIDGE = "h_bridge"
@@ -33,53 +57,128 @@ class TestHW(Enum):
     def __str__(self):
         return self.value
 
-###YAML PARSER
+def batteryCallback(data):
+    batteryData = data
+    isNewBatteryData = 1
 
-def parse_yaml(file_path):
-    with open(file_path, "r") as stream:
-        try:
-            return yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
+def imuCallback(data): 
+    imuData = data
+    isNewImuData = 1
 
-    return tuple()
+def wheelCallback(data):
+    wheelData = data
+    isNewWheelData = 1
+
+    #####################################################
+
+###Publisher
+
+cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+
+cmd_pwmFL_pub = rospy.Publisher('firmware/wheel_FL/cmd_pwm_duty', Float32, queue_size=1)
+cmd_pwmRL_pub = rospy.Publisher('firmware/wheel_RL/cmd_pwm_duty', Float32, queue_size=1)
+cmd_pwmFR_pub = rospy.Publisher('firmware/wheel_FR/cmd_pwm_duty', Float32, queue_size=1)
+cmd_pwmRR_pub = rospy.Publisher('firmware/wheel_RR/cmd_pwm_duty', Float32, queue_size=1)
+
+###Subscriber
+
+battery_sub = rospy.Subscriber('firmware/battery', Float32, batteryCallback)
+wheel_sub = rospy.Subscriber('firmware/wheel_state', WheelStates, wheelCallback)
+imu_sub = rospy.Subscriber('firmware/imu', Imu, imuCallback)
+
+###WHEEL LOAD TEST
+
+def check_motor_load():
+
+    for x in range(1,20):
+
+        cmd_pwmFL_pub.publish(Float32(x))
+        cmd_pwmFR_pub.publish(Float32(x))
+        cmd_pwmRL_pub.publish(Float32(x))
+        cmd_pwmRR_pub.publish(Float32(x))
+
+        rospy.sleep(0.1)
 
 ###MOTOR ENCODER TEST
 
 def check_encoder():
-    path = rospack.get_path('leo_fw')
-    encoder_valid = parse_yaml(path+"/validate/motor.yaml")
-    print(encoder_valid)
+    print(motor_valid)
 
 ###MOTOR TORQUE TEST
 
 def check_torque():
-    path = rospack.get_path('leo_fw')
-    torque_valid = parse_yaml(path+"/validate/motor.yaml")
-    print(torque_valid)
+    print(motor_valid)
 
 ###IMU TEST
 
 def check_imu():
-    path = rospack.get_path('leo_fw')
+    msg_cnt = 0
+    time_now = time.time()
     imu_valid = parse_yaml(path+"/validate/imu.yaml")
-    print(imu_valid)
-    write_flush("--> Checking IMU.. ")
 
-    
+    accel_del = imu_valid["imu"]["accel_del"]
+    accel_x = imu_valid["imu"]["accel_x"]
+    accel_y = imu_valid["imu"]["accel_y"]
+    accel_z = imu_valid["imu"]["accel_z"]
+
+    gyro_del = imu_valid["imu"]["gyro_del"]
+    gyro_x = imu_valid["imu"]["gyro_x"]
+    gyro_y = imu_valid["imu"]["gyro_y"]
+    gyro_z = imu_valid["imu"]["gyro_z"]
+
+    while(msg_cnt<50):
+        if (time_now+imu_valid["imu"]["timeout"] < time.time()):
+            print("TIMEOUT")
+            return 0
+
+        if (isNewImuData == 1):
+            time_now = time.time()
+            isNewImuData = 0
+            msg_cnt += 1
+
+            if(accel_x-accel_del > imuData.accel_x > accel_x+accel_del or 
+            accel_y-accel_del > imuData.accel_y > accel_y+accel_del or
+            accel_z-accel_del > imuData.accel_z > accel_z+accel_del or
+            gyro_x-gyro_del > imuData.gyro_x > gyro_x+gyro_del or
+            gyro_y-gyro_del > imuData.gyro_y > gyro_y+gyro_del or
+            gyro_z-gyro_del > imuData.gyro_z > gyro_z+gyro_del):
+                print("INVALID DATA")
+                return 0
+
+    print("PASSED")
+    return 1    
 
 ###BATTERY TEST
 
-def check_battery():
-    path = rospack.get_path('leo_fw')
-    batt_valid = parse_yaml(path+"/validate/battery.yaml")
-    print(batt_valid)
+def check_battery(): 
+    msg_cnt = 0
+    time_now = time.time()
+    batt_valid = parse_yaml(path+"/validate/battery.yaml") 
+
+    while(msg_cnt<50):
+        if (time_now+batt_valid["battery"]["timeout"] < time.time()):
+            print("TIMEOUT")
+            return 0
+
+        if (isNewBatteryData == 1):
+            time_now = time.time()
+            isNewBatteryData = 0
+            msg_cnt += 1
+
+            if(batteryData.data <= batt_valid["battery"]["voltage_min"]):
+                print("LOW VOLTAGE")
+                return 0
+            elif(batteryData.data >= batt_valid["battery"]["voltage_max"]):
+                print("HIGH VOLTAGE")
+                return 0
+
+    print("PASSED")
+    return 1
 
 #####################################################
 
 def validate_hw(
     hardware: Optional[TestHW] = TestHW.ALL,
-    mode: Optional[TestMode] = TestMode.NO_WHEEL,
     rosbag: bool = False,
 ):
 
@@ -173,14 +272,22 @@ def validate_hw(
         return
 
     #####################################################
+    
+    if (hardware==TestHW.ALL or hardware==TestHW.BATTERY):
+        write_flush("--> Battery validation.. ")
+        check_battery()
+    
+    if (hardware==TestHW.ALL or hardware==TestHW.IMU):
+        write_flush("--> IMU validation.. ")
+        check_imu()
 
-    check_encoder()
+    if (hardware==TestHW.ALL or hardware==TestHW.ENCODER): 
+        write_flush("--> Encoders validation.. ")
+        check_encoder()
 
-    check_torque()
-
-    check_imu()
-
-    check_battery()
+    if (hardware==TestHW.ALL or hardware==TestHW.TORQUE):
+        write_flush("--> Torque sensors validation.. ")
+        check_torque()
   
     #####################################################
 
