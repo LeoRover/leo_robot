da7486259c4e95558e438bdbed764c4ff3f4323a
Aleksander Szymański <bitterisland6@gmail.com>
2023-07-07 18:11:04 +0200

    include firmware/wheel_odom_mecanum topic handling in message converter
    Signed-off-by: Aleksander Szymański <bitterisland6@gmail.com>

--- leo_fw/src/firmware_message_converter.cpp
+++ leo_fw/src/firmware_message_converter.cpp
@@ -14,6 +14,7 @@
 #include "leo_msgs/Imu.h"
 #include "leo_msgs/WheelOdom.h"
 #include "leo_msgs/WheelStates.h"
+#include "leo_msgs/MecanumOdom.h"
 
 #include "leo_msgs/SetImuCalibration.h"
 
@@ -24,6 +25,8 @@ static bool joint_states_advertised = false;
 static ros::Subscriber wheel_odom_sub;
 static ros::Publisher wheel_odom_pub;
 static bool wheel_odom_advertised = false;
+static bool wheel_odom_mecanum_advertised = false;
+static ros::Subscriber wheel_odom_mecanum_sub;
 
 static ros::Subscriber imu_sub;
 static ros::Publisher imu_pub;
@@ -50,6 +53,9 @@ std::vector<std::string> wheel_joint_names = {
     "wheel_FL_joint", "wheel_RL_joint", "wheel_FR_joint", "wheel_RR_joint"};
 std::vector<double> wheel_odom_twist_covariance_diagonal = {0.0001, 0.0, 0.0,
                                                             0.0,    0.0, 0.001};
+std::vector<double> mecanum_wheel_odom_twist_covariance_diagonal = {0.0001, 0.0001, 0.0,
+                                                            0.0,    0.0, 0.001};
+
 std::vector<double> imu_angular_velocity_covariance_diagonal = {
     0.000001, 0.000001, 0.00001};
 std::vector<double> imu_linear_acceleration_covariance_diagonal = {0.001, 0.001,
@@ -66,6 +72,8 @@ void load_parameters(ros::NodeHandle& pnh) {
   pnh.getParam("wheel_joint_names", wheel_joint_names);
   pnh.getParam("wheel_odom_twist_covariance_diagonal",
                wheel_odom_twist_covariance_diagonal);
+  pnh.getParam("mecanum_wheel_odom_twist_covariance_diagonal",
+               mecanum_wheel_odom_twist_covariance_diagonal);
   pnh.getParam("imu_angular_velocity_covariance_diagonal",
                imu_angular_velocity_covariance_diagonal);
   pnh.getParam("imu_linear_acceleration_covariance_diagonal",
@@ -103,8 +111,7 @@ void wheel_odom_callback(const leo_msgs::WheelOdomPtr& msg) {
   wheel_odom.pose.pose.orientation.z = std::sin(msg->pose_yaw * 0.5F);
   wheel_odom.pose.pose.orientation.w = std::cos(msg->pose_yaw * 0.5F);
 
-  velocity_linear_x = msg->velocity_lin_x;
-  velocity_linear_y = msg->velocity_lin_y;
+  velocity_linear_x = msg->velocity_lin;
 
   for (int i = 0; i < 6; i++)
     wheel_odom.twist.covariance[i * 7] =
@@ -112,6 +119,7 @@ void wheel_odom_callback(const leo_msgs::WheelOdomPtr& msg) {
 
   wheel_odom_pub.publish(wheel_odom);
 }
+
 void imu_callback(const leo_msgs::ImuPtr& msg) {
   sensor_msgs::Imu imu;
   imu.header.frame_id = imu_frame_id;
@@ -133,6 +141,29 @@ void imu_callback(const leo_msgs::ImuPtr& msg) {
   }
 
   imu_pub.publish(imu);
+}
+
+void mecanum_odom_callback(const leo_msgs::MecanumOdomPtr& msg) {
+  nav_msgs::Odometry wheel_odom;
+  wheel_odom.header.frame_id = odom_frame_id;
+  wheel_odom.child_frame_id = robot_frame_id;
+  wheel_odom.header.stamp = msg->stamp;
+  wheel_odom.twist.twist.linear.x = msg->velocity_lin_x;
+  wheel_odom.twist.twist.linear.y = msg->velocity_lin_y;
+  wheel_odom.twist.twist.angular.z = msg->velocity_ang;
+  wheel_odom.pose.pose.position.x = msg->pose_x;
+  wheel_odom.pose.pose.position.y = msg->pose_y;
+  wheel_odom.pose.pose.orientation.z = std::sin(msg->pose_yaw * 0.5F);
+  wheel_odom.pose.pose.orientation.w = std::cos(msg->pose_yaw * 0.5F);
+
+  velocity_linear_x = msg->velocity_lin_x;
+  velocity_linear_y = msg->velocity_lin_y;
+
+  for (int i = 0; i < 6; i++)
+    wheel_odom.twist.covariance[i * 7] =
+        mecanum_wheel_odom_twist_covariance_diagonal[i];
+
+  wheel_odom_pub.publish(wheel_odom);
 }
 
 void merge_odometry_callback(const ros::TimerEvent& events){
@@ -252,6 +283,8 @@ int main(int argc, char** argv) {
       ros::names::resolve("firmware/wheel_states");
   const std::string wheel_odom_topic =
       ros::names::resolve("firmware/wheel_odom");
+  const std::string wheel_odom_mecanum_topic =
+      ros::names::resolve("firmware/wheel_odom_mecanum");
   const std::string imu_topic = ros::names::resolve("firmware/imu");
 
   set_imu_calibration_service =
@@ -279,6 +312,17 @@ int main(int argc, char** argv) {
       odom_merged_pub.shutdown();
       odom_merged_timer.stop();
       wheel_odom_advertised = false;
+      odom_merged_advertised = false;
+    }
+    if (wheel_odom_mecanum_advertised && wheel_odom_mecanum_sub.getNumPublishers() == 0) {
+      ROS_INFO(
+          "firmware/wheel_odom_mecanum topic no longer has any publishers. "
+          "Shutting down wheel_odom_with_covariance and odometry_merged publishers.");
+      wheel_odom_mecanum_sub.shutdown();
+      wheel_odom_pub.shutdown();
+      odom_merged_pub.shutdown();
+      odom_merged_timer.stop();
+      wheel_odom_mecanum_advertised = false;
       odom_merged_advertised = false;
     }
     if (imu_advertised && imu_sub.getNumPublishers() == 0) {
@@ -320,6 +364,16 @@ int main(int argc, char** argv) {
             nh.subscribe("firmware/wheel_odom", 5, wheel_odom_callback);
         wheel_odom_advertised = true;
       }
+      if (!wheel_odom_mecanum_advertised && topic.name == wheel_odom_mecanum_topic) {
+        ROS_INFO(
+            "Detected firmware/wheel_odom_mecanum topic advertised. "
+            "Advertising wheel_odom_with_covariance topic.");
+        wheel_odom_pub =
+            nh.advertise<nav_msgs::Odometry>("wheel_odom_with_covariance", 10);
+        wheel_odom_mecanum_sub =
+            nh.subscribe("firmware/wheel_odom_mecanum", 5, mecanum_odom_callback);
+        wheel_odom_mecanum_advertised = true;
+      }
       if (!imu_advertised && topic.name == imu_topic) {
         ROS_INFO(
             "Detected firmware/imu topic advertised. "
@@ -328,7 +382,6 @@ int main(int argc, char** argv) {
         imu_sub = nh.subscribe("firmware/imu", 5, imu_callback);
         imu_advertised = true;
       }
-
       if(!odom_merged_advertised && imu_advertised && wheel_odom_advertised) {
         ROS_INFO(
             "Both firmware/imu and firmware/wheel_odom topics are advertised. "
